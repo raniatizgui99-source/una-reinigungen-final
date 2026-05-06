003c0496cff4742cdaa46b12fef5439291c78b4a
raniatizgui99-source <raniatizgui99@gmail.com>
2026-05-06 17:22:53 +0200

    feat: install Google Tag Manager (GTM-58MLX63X)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,15 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-58MLX63X');`
+        }} />
+        {/* End Google Tag Manager */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11462828452"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
@@ -25,6 +34,12 @@ export default function RootLayout({
         }} />
       </head>
       <body className="bg-[#fafafa] text-slate-900 selection:bg-slate-900 selection:text-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58MLX63X"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
